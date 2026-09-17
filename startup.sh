9f6daa282be93516e8f4070c4e9454f621b11352
Scot <scot@scotnpatti.com>
2026-09-17 12:51:49 -0400

    Fixed the start to also trust the proxy and bind to any IP since only the Azure proxy will be allowed to talk to the web app, and that is trustworthy too.

--- startup.sh
+++ startup.sh
@@ -3,4 +3,4 @@ set -e
 echo "Running database migrations..."
 flask db upgrade
 echo "Starting gunicorn..."
-exec gunicorn app:app --bind 0.0.0.0:8000 --workers 2 --timeout 120
+exec gunicorn app:app --bind 0.0.0.0:8000 --workers 2 --timeout 120 --forwarded-allow-ips="*"
